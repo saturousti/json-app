14eea794b1b5b29a3bb6e5561b23c47cafd60222
saturousti <118724002+saturousti@users.noreply.github.com>
2025-10-24 18:11:10 +0300

    Update trend-scatter.ts

--- src/app/trend-scatter/trend-scatter.ts
+++ src/app/trend-scatter/trend-scatter.ts
@@ -2,7 +2,7 @@ import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import Chart from 'chart.js/auto';
-import { TrendResults } from '../../models';
+import { DemoResults } from '../../models';
 
 
 @Component({
@@ -19,49 +19,54 @@ export class TrendScatter implements AfterViewInit {
   constructor(private http: HttpClient) {}
 
   ngAfterViewInit() {
-    this.http.get<TrendResults>('/assets/data/demo_result.json').subscribe(data => {
+    this.http.get<DemoResults>('/assets/data/demo_result.json').subscribe(data => {
 
-      const hrvAvg = data.HRTrend.hrv.average;
-      const rateAvg = data.HRTrend.rate.average;
-      const pulsAvg = data.PulsatilityTrend.average;
-      const gIndexAvg = data.GIndexTrend.average;
       //const RateTrend = data.HRTrend.rate;
-     const dataPoints = [
-  { name: 'Heart Rate', value: rateAvg },
-  { name: 'HRV', value: hrvAvg },
-  { name: 'Pulsatility', value: pulsAvg },
-  { name: 'G Index', value: gIndexAvg }
-];
+      const hrvAvg = data.heartrate.trends.hrv.average;
+      const rateAvg = data.heartrate.trends.rate.average;
+      const pulsAvg = data.brainPulsatility.trends.average;
+      const gIndexAvg = data.gIndex.trends.average;
 
-new Chart(this.chartRef.nativeElement, {
-  type: 'scatter',
-  data: {
-    datasets: [{
-      label: 'Trends',
-      data: dataPoints.map(p => ({ x: p.value, y: p.value })),
-      backgroundColor: 'rgba(225, 59, 95, 1)'
-    }]
-  },
-  options: {
-    scales: {
-      x: {
-        type: 'linear',
-        position: 'bottom',
-        title: {
-          display: true,
-          text: 'Keskiarvo'
-        }
-      },
-      y: {
-        title: {
-          display: true,
-          text: 'Keskiarvo'
-        }
-      }
-    }
-  }
-});
-                                         
+
+      new Chart(this.chartRef.nativeElement, {
+                     type: 'scatter',
+                     data: {
+                      datasets: [{
+                      label: 'Trends',
+                      data: [{
+                        label: 'Heart Rate Trend',
+                        x: rateAvg,
+                        y: rateAvg,
+                        backgroundColor: 'rgba(42, 216, 68, 1)',
+                      }, {
+                        label: 'HRV Trend',
+                        x: hrvAvg,
+                        y: hrvAvg,
+                        backgroundColor: 'rgba(225, 134, 59, 1)'
+                      }, {
+                        label: 'Brain Pulsatility Trend',
+                        x: pulsAvg,
+                        y: pulsAvg,
+                        backgroundColor: 'rgba(225, 59, 84, 1)'
+                      }, {
+                        label: 'G Index Trend',
+                        x: gIndexAvg,
+                        y: gIndexAvg,
+                        backgroundColor: 'rgba(59, 181, 225, 1)',
+                        
+                      }],
+                      //backgroundColor: 'rgba(225, 59, 95, 1)'
+                      }],
+                    },
+                    options: {
+                      scales: {
+                        x: {
+                        type: 'linear',
+                        position: 'bottom'
+                        }
+                      }
+                    }
+        });                                          
     });
   }
 }
